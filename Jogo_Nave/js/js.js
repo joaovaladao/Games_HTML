df213433cfd020c5afcd3471496fd264f3411656
joaovaladao <joaovaladao161274@gmail.com>
2022-06-22 13:31:47 -0300

    Movimentação do jogador usando js

--- Jogo_Nave/js/js.js
+++ Jogo_Nave/js/js.js
@@ -10,13 +10,44 @@ function start() { // Inicio da função start()
     var jogo = {};
     jogo.timer = setInterval(loop,30);
 
+    var TECLA = {
+        W: 87,
+        S: 83,
+        space: 8
+    }
+    jogo.pressionou = [];
+
+    //Verifica se o usuário pressionou alguma tecla		
+	$(document).keydown(function(e){
+        jogo.pressionou[e.which] = true;
+    });
+    
+    
+    $(document).keyup(function(e){
+        jogo.pressionou[e.which] = false;
+    });
+
     function loop(){
         movefundo();
+        movejogador();
     }
 
     function movefundo(){
         pos = parseInt($("#fundoGame").css("background-position"));
         $("#fundoGame").css("background-position",pos-1);
     }
+
+    function movejogador(){
+        if(jogo.pressionou[TECLA.W]){
+            var top = parseInt($("#jogador").css("top"));
+            $("#jogador").css("top",top-10);
+        }
+
+        else if(jogo.pressionou[TECLA.S]){
+            var top = parseInt($("#jogador").css("top"));
+            $("#jogador").css("top",top+10);
+        }
+    }
+    
 
 } // Fim da função start
