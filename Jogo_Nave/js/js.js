8a46ea7879b0d80c3b0531ea2218a237b48abe04
joaovaladao <joaovaladao161274@gmail.com>
2022-06-22 13:19:36 -0300

    Animação do plano de fundo

--- Jogo_Nave/js/js.js
+++ Jogo_Nave/js/js.js
@@ -7,4 +7,16 @@ function start() { // Inicio da função start()
 	$("#fundoGame").append("<div id='inimigo2' ></div>");
 	$("#fundoGame").append("<div id='amigo' class='anima3'></div>");
 
+    var jogo = {};
+    jogo.timer = setInterval(loop,30);
+
+    function loop(){
+        movefundo();
+    }
+
+    function movefundo(){
+        pos = parseInt($("#fundoGame").css("background-position"));
+        $("#fundoGame").css("background-position",pos-1);
+    }
+
 } // Fim da função start
