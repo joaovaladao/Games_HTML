448b6e0af8a99f6bcff8595f8e2b6c6284f4e2c4
joaovaladao <joaovaladao161274@gmail.com>
2022-06-23 11:34:01 -0300

    Implementação de limites e movimentação do primeiro inimigo

--- Jogo_Nave/js/js.js
+++ Jogo_Nave/js/js.js
@@ -17,6 +17,9 @@ function start() { // Inicio da função start()
     }
     jogo.pressionou = [];
 
+    var velocidade = 5;
+    var pos_y = parseInt(Math.random() * 335);
+
     //Verifica se o usuário pressionou alguma tecla		
 	$(document).keydown(function(e){
         jogo.pressionou[e.which] = true;
@@ -30,6 +33,7 @@ function start() { // Inicio da função start()
     function loop(){
         movefundo();
         movejogador();
+        moveinimigo1();
     }
 
     function movefundo(){
@@ -41,11 +45,31 @@ function start() { // Inicio da função start()
         if(jogo.pressionou[TECLA.W]){
             var top = parseInt($("#jogador").css("top"));
             $("#jogador").css("top",top-10);
+
+            if(top <= 0){
+                $("#jogador").css("top",top+10);
+            }
         }
 
         else if(jogo.pressionou[TECLA.S]){
             var top = parseInt($("#jogador").css("top"));
             $("#jogador").css("top",top+10);
+
+            if(top >= 435){
+                $("#jogador").css("top",top-10);
+            }
+        }
+    }
+
+    function moveinimigo1(){
+        pos_x = parseInt($("#inimigo1").css("left"));
+        $("#inimigo1").css("left", pos_x - velocidade);
+        $("#inimigo1").css("top", pos_y);
+
+        if(pos_x <= 0){
+            pos_y = parseInt(Math.random() * 335);
+            $("#inimigo1").css("left", 694);
+            $("#inimigo1").css("top", pos_y);
         }
     }
     
