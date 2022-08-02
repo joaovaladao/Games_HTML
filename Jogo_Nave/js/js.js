c7934f9135f2c2ca07a9758f1e8de506aacae452
joaovaladao <joaovaladao161274@gmail.com>
2022-08-02 16:16:52 -0300

    Animação de colisão aliado e inimigo

--- Jogo_Nave/js/js.js
+++ Jogo_Nave/js/js.js
@@ -203,6 +203,18 @@ function start() { // Inicio da função start()
                 reposicionaAmigo();
                 $("#amigo").remove();
             }
+
+            if (colisao6.length>0) {
+                console.log("colisão6");
+
+                amigoX = parseInt($("#amigo").css("left"));
+                amigoY = parseInt($("#amigo").css("top"));
+                explosao3(amigoX,amigoY);
+                $("#amigo").remove();
+                        
+                reposicionaAmigo();
+                        
+                }
         
         }
 
@@ -245,6 +257,24 @@ function start() { // Inicio da função start()
                 
             }
 
+    }
+
+    function explosao3(amigoX,amigoY) {
+
+        $("#fundoGame").append("<div id='explosao3' class='anima4'></div");
+        $("#explosao3").css("top",amigoY);
+        $("#explosao3").css("left",amigoX);
+
+        var tempoExplosao3=window.setInterval(resetaExplosao3, 1000);
+
+            function resetaExplosao3() {
+
+                $("#explosao3").remove();
+                window.clearInterval(tempoExplosao3);
+                tempoExplosao3=null;
+                    
+            }
+        
     }
 
     function reposicionaInimigo2() {
